c83a25f2d3e2c8d935e79baae67437b009e003a3
rssr25 <rahulsharmacs50@gmail.com>
2019-11-23 18:38:02 +0530

    Done with two objects rendering using uniforms

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -41,10 +41,10 @@ int main(void)
 
 	{
 		float positions[] = {
-			100.0f, 100.0f, 0.0f, 0.0f, //0
-			200.0f, 100.0f, 1.0f, 0.0f, //1
-			200.0f, 200.0f, 1.0f, 1.0f, //2
-			100.0f, 200.0f, 0.0f, 1.0f  //3
+			-50.0f, -50.0f, 0.0f, 0.0f, //0
+			 50.0f, -50.0f, 1.0f, 0.0f, //1
+			 50.0f,  50.0f, 1.0f, 1.0f, //2
+			-50.0f,  50.0f, 0.0f, 1.0f  //3
 		};
 
 		unsigned int indices[] =
@@ -70,7 +70,7 @@ int main(void)
 
 		//intro to projection, we just have an orthographic projection matrix here
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f); //4:3 aspect ratio
-		glm::mat4 view = glm::translate(glm::mat4(1.0), glm::vec3(-100, 0, 0));
+		glm::mat4 view = glm::translate(glm::mat4(1.0), glm::vec3(0, 0, 0));
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
@@ -92,7 +92,8 @@ int main(void)
 		ImGui_ImplOpenGL3_Init("#version 130");
 		ImGui::StyleColorsDark();
 
-		glm::vec3 translation(200, 200, 0);
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 200, 0);
 		float r = 0.0f;
 		float increment = 0.05f;
 
@@ -106,14 +107,24 @@ int main(void)
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
 			
-			glm::mat4 model = glm::translate(glm::mat4(1.0), translation);
-			glm::mat4 mvp = proj * view * model; 
 
-			shader.Bind(); 
-			shader.setUniform4f("u_Color", r, 0.0f, 0.5f, 1.0f);
-			shader.setUniformMat4f("u_MVP", mvp);
 
-			renderer.Draw(va, ib, shader);
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0), translationA);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.setUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
+			
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.setUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
+			
 			
 
 			if (r > 1.0f)
@@ -128,7 +139,8 @@ int main(void)
 				static int counter = 0;
 
 				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into i
-				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0
+				ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);
+				ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 				ImGui::End();
 			}
